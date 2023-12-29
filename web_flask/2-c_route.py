a4f0bf9d14d43c71adcb86dcf867153aac092a9d
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-12-29 07:09:48 +0100

    add solutions

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+ Starts a new flask application
+ """
+
+from flask import Flask
+
+
+# Create a Flask web application
+app = Flask(__name__)
+
+
+# Define a route for the root URL
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_is_fun(text):
+    modify = text.replace('_', ' ')
+    return f'C {modify}'
+
+
+# Run the web application on 0.0.0.0:5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
