cfa6a6e05dc8de6bc296a875179ad3c713d41fb9
Ahmed Olalekan <olalekanahmed6@gmail.com>
2023-12-29 09:11:43 +0100

    add solutions

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+"""
+ Starts a new flask application
+ """
+
+from flask import Flask
+from flask import render_template
+
+# Create a Flask web application
+app = Flask(__name__)
+
+
+# Define a route for the root URL
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_is_fun(text):
+    modify = text.replace('_', ' ')
+    return f'C {modify}'
+
+
+@app.route('/python/<string:text>/', strict_slashes=False)
+@app.route('/python/', strict_slashes=False)
+def python_is_cool(text='is cool'):
+    modify = text.replace('_', ' ')
+    return f'Python {modify}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def ren_template(n):
+    return render_template('5-number.html', var=n)
+
+
+# Run the web application on 0.0.0.0:5000
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
